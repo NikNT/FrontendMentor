4f11fe532959345135503e8058e204e122309a52
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-08-10 23:11:35 -0400

    3. AgeCalc - Error State Done | Mobile Reponsive - Needs Retouch

--- 3. AgeCalc/agecalc/src/Components/Age.jsx
+++ 3. AgeCalc/agecalc/src/Components/Age.jsx
@@ -17,44 +17,105 @@ const Age = () => {
 
   const [emptyForm, setEmptyForm] = useState(false);
 
+  const [validateInput, setValidateInput] = useState({
+    isValid: true,
+    day: true,
+    month: true,
+    year: true,
+  });
+
+  const [validityOfCompleteDate, setValidityOfCompleteDate] = useState(true);
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setInput((prevState) => ({
       ...prevState,
       [name]: value,
     }));
+
+    if (emptyForm) {
+      setEmptyForm(false);
+    }
+  };
+
+  const checkCompleteDate = (year, month, day) => {
+    const date = new Date(year, month - 1, day);
+    return (
+      date.getFullYear() === parseInt(year, 10) &&
+      date.getMonth() === parseInt(month, 10) - 1 &&
+      date.getDate() === parseInt(day, 10)
+    );
   };
 
   const handleClick = () => {
+    const date = new Date();
+    // Check I - Input fields are empty
     if (input.day === "" && input.month === "" && input.year === "") {
       setEmptyForm(true);
       console.log("Empty Form");
       return;
     }
 
-    const date = new Date();
+    //Check II - Entered Date is Valid or Not
+    const checkDate = checkCompleteDate(input.year, input.month, input.day);
 
-    const today = date.getDate();
-    const currentMonth = date.getMonth() + 1;
-    const currentYear = date.getFullYear();
+    if (!checkDate) {
+      setValidityOfCompleteDate(false);
+      console.log("Wrong Date");
+      return;
+    }
 
-    const calculateDay =
-      input.day < today ? today - input.day : input.day - today;
-    const calculateMonth =
-      input.month < currentMonth ? currentMonth - input.month : 0;
-    const calculateYear = currentYear - input.year;
+    // Check III - Either Day | Month | Year field is empty
 
-    setOutput({
-      day: calculateDay,
-      month: calculateMonth,
-      year: calculateYear,
-    });
+    const isValidDay = parseInt(input.day) >= 1 && parseInt(input.day) <= 31;
+    const isValidMonth =
+      parseInt(input.month) >= 1 && parseInt(input.month) <= 12;
+    const isValidYear =
+      parseInt(input.year) >= 1900 &&
+      parseInt(input.year) <= date.getFullYear();
 
-    setInput({
-      day: "",
-      month: "",
-      year: "",
-    });
+    console.log("Day", isValidDay);
+    console.log("Month", isValidMonth);
+    console.log("Year", isValidYear);
+
+    setValidateInput((prevValidateInput) => ({
+      ...prevValidateInput,
+      isValid: isValidDay && isValidMonth && isValidYear,
+      day: isValidDay,
+      month: isValidMonth,
+      year: isValidYear,
+    }));
+
+    console.log("is Valid", validateInput.isValid);
+
+    if (!isValidDay || !isValidMonth || !isValidYear) {
+      return;
+    } else {
+      const today = date.getDate();
+      const currentMonth = date.getMonth() + 1;
+      const currentYear = date.getFullYear();
+
+      const calculateDay =
+        input.day < today ? today - input.day : input.day - today;
+      const calculateMonth =
+        input.month < currentMonth ? currentMonth - input.month : 0;
+      const calculateYear = currentYear - input.year;
+
+      setOutput({
+        day: calculateDay,
+        month: calculateMonth,
+        year: calculateYear,
+      });
+
+      setInput({
+        day: "",
+        month: "",
+        year: "",
+      });
+
+      setEmptyForm(false);
+      setValidityOfCompleteDate(true);
+    }
   };
 
   const fieldRequired = (
@@ -67,7 +128,12 @@ const Age = () => {
         <div className={styles.inside}>
           <div className={styles.form}>
             <div className={styles.form_element}>
-              <label htmlFor="day" className={emptyForm && styles.empty_value}>
+              <label
+                htmlFor="day"
+                className={
+                  (emptyForm || !validateInput.day) && styles.empty_value
+                }
+              >
                 D a y
               </label>
               <input
@@ -76,15 +142,25 @@ const Age = () => {
                 placeholder="DD"
                 value={input.day}
                 onChange={handleInputChange}
-                className={emptyForm ? styles.error_border : ""}
+                className={
+                  emptyForm || !validateInput.day ? styles.error_border : ""
+                }
               />
               {emptyForm && fieldRequired}
+              {!validateInput.day && (
+                <h6 className={styles.field_required}>Must be a valid day</h6>
+              )}
+              {!validityOfCompleteDate && (
+                <h6 className={styles.field_required}>Must be a valid date</h6>
+              )}
             </div>
 
             <div className={styles.form_element}>
               <label
                 htmlFor="month"
-                className={emptyForm && styles.empty_value}
+                className={
+                  (emptyForm || !validateInput.month) && styles.empty_value
+                }
               >
                 M o n t h
               </label>
@@ -94,13 +170,23 @@ const Age = () => {
                 placeholder="MM"
                 value={input.month}
                 onChange={handleInputChange}
-                className={emptyForm ? styles.error_border : ""}
+                className={
+                  emptyForm || !validateInput.month ? styles.error_border : ""
+                }
               />
               {emptyForm && fieldRequired}
+              {!validateInput.month && (
+                <h6 className={styles.field_required}>Must be a valid month</h6>
+              )}
             </div>
 
             <div className={styles.form_element}>
-              <label htmlFor="year" className={emptyForm && styles.empty_value}>
+              <label
+                htmlFor="year"
+                className={
+                  (emptyForm || !validateInput.year) && styles.empty_value
+                }
+              >
                 Y e a r
               </label>
               <input
@@ -109,9 +195,14 @@ const Age = () => {
                 placeholder="YYYY"
                 value={input.year}
                 onChange={handleInputChange}
-                className={emptyForm ? styles.error_border : ""}
+                className={
+                  emptyForm || !validateInput.year ? styles.error_border : ""
+                }
               />
               {emptyForm && fieldRequired}
+              {!validateInput.year && (
+                <h6 className={styles.field_required}>Must be a valid year</h6>
+              )}
             </div>
           </div>
           <div className={styles.image}>
