e5200fe9933a51d131132bd087bda605336de8b8
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-08-12 09:36:01 -0400

    4. Advice Generator - Test

--- 4. Advice Generator/advicegenerator/src/Components/Advice.jsx
+++ 4. Advice Generator/advicegenerator/src/Components/Advice.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Advice.module.css";
 import dice from "../Assets/icon-dice.svg";
+import desktopPattern from "../Assets/pattern-divider-desktop.svg";
+import mobilePattern from "../Assets/pattern-divider-mobile.svg";
 
 const Advice = () => {
   const [advice, setAdvice] = useState({
@@ -8,7 +10,8 @@ const Advice = () => {
     quote: "",
   });
 
-  const [buttonClick, setButtonClick] = useState(false);
+  const [buttonClick, setButtonClick] = useState(0);
+  const [view, setView] = useState(window.innerWidth);
 
   useEffect(() => {
     fetch("https://api.adviceslip.com/advice")
@@ -23,15 +26,39 @@ const Advice = () => {
   }, [buttonClick]);
 
   const generateNewQuote = () => {
-    setButtonClick(!buttonClick);
+    setButtonClick(buttonClick + 1);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setView(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.wrapper}>
-          <div className={styles.id}> A D V I C E # {advice.id} </div>
-          <div className={styles.quote}> {advice.quote}</div>
+          <div className={styles.card}>
+            <div className={styles.id}>
+              <p> ADVICE #{advice.id} </p>
+            </div>
+            <div className={styles.quote}>
+              <p> “{advice.quote}” </p>
+            </div>
+            <div className={styles.image}>
+              <img
+                src={view >= 393 ? desktopPattern : mobilePattern}
+                alt="Divider"
+              />
+            </div>
+          </div>
           <div className={styles.dice}>
             <img src={dice} alt="Dice" onClick={generateNewQuote} />
           </div>
