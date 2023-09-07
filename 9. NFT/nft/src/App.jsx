7b4f29d5be404c7e3943530db95c6972abd7dbee
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-09-06 21:31:55 -0400

    9. NFT - Final | Minor Changes -> Might be Required

---
+++ 9. NFT/nft/src/App.jsx
@@ -0,0 +1,48 @@
+import "./index.css";
+import equillibrium from "./assets/image-equilibrium.jpg";
+import eth from "./assets/icon-ethereum.svg";
+import clock from "./assets/icon-clock.svg";
+import avatar from "./assets/image-avatar.png";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="card">
+          <figure className="image">
+            <img src={equillibrium} alt="Equillibrium" />
+          </figure>
+
+          <div className="title">
+            <h1>Equilibrium #3429</h1>
+          </div>
+
+          <main>
+            <p>Our Equilibrium collection promtes balance and calm.</p>
+          </main>
+
+          <section className="values">
+            <div className="price">
+              <img src={eth} alt="Eth" className="price-image" />
+              <span className="price-text">0.041 ETH</span>
+            </div>
+            <div className="days">
+              <img src={clock} alt="Eth" className="days-image" />
+              <span className="days-text">3 days left</span>
+            </div>
+          </section>
+
+          <hr className="hr" />
+          <footer>
+            <img src={avatar} alt="Avatar" />
+            <p>
+              <span> Creation of </span> Jules Wyvern
+            </p>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
